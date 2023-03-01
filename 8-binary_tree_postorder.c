9cffd7f1c1b4bf40b833568e9da0299ac62e8b7c
Funom3 <kingsleyisaackushang1997@gmail.com>
2023-03-01 12:49:20 -0800

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_postorder - goes through binary tree using post-order traversal
+ * binary_tree_postorder - goes to binary tree using post-order traversal
  *
  * @tree: the node
  *
- * @func: function to print
+ * @func: the function to print
  *
  * Return: always 0
  */
@@ -13,7 +12,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 	func(tree->n);
 }
