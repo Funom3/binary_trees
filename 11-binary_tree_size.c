c41db9e5f0d40e9f0fa7b9a2b11651f9aee20384
Funom3 <kingsleyisaackushang1997@gmail.com>
2023-03-01 13:07:22 -0800

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,10 +10,11 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t s = 1;
 
-	if (!tree)
+	if (tree)
 	{
-		return (0);
-		s = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
+		s += 1;
+		s += binary_tree_size(tree->left);
+		s += binary_tree_size(tree->right);
 	}
 	return (s);
 }
